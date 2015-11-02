6d16e943efcebea0a2278df118f1306f8f1b79d6
R. Alex Barbieri <luck@mit.edu>
2015-11-02 15:50:37 +0100

    update to new runs

--- crabConfig.py
+++ crabConfig.py
@@ -14,12 +14,12 @@ config.JobType.psetName = "runUnpackerReEmulatorAnalyzer.py"
 config.Data.inputDataset = "/ExpressPhysics/Run2015D-Express-v4/FEVT"
 #config.Data.inputDBS = "phys03"
 config.Data.splitting = "LumiBased"
-config.Data.runRange = "260350,260422-260433"
+config.Data.runRange = "260593,260577,260576,260538,260536,260534,260533,260527,260510,260490"
 config.Data.unitsPerJob = 20
 config.Data.totalUnits = -1
 
 config.Data.outLFNDirBase = "/store/user/richard/L1Emulator/20151031/75X/"
-config.Data.publishDataName = "PP_Physics_38T_HIFW_SecondBoard_RUNS_v2"
+config.Data.publishDataName = "PP_Physics_38T_HIFW_SecondBoard_RUNS_group2"
 
 #### Site ####
 config.Site.storageSite = "T2_US_MIT"
