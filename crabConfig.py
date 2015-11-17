c3fffb1f89603c5a0b2055fc118f80cb31712f3d
R. Alex Barbieri <luck@mit.edu>
2015-11-17 17:22:05 +0100

    switch from relval to larger dataset

--- crabConfig.py
+++ crabConfig.py
@@ -8,22 +8,23 @@ config = config()
 
 #### JobType ####
 config.JobType.pluginName = "Analysis"
-config.JobType.psetName = "step2_RAW2DIGI_L1Reco_RECO.py"
+config.JobType.psetName = "step3_RAW2DIGI_L1Reco_RECO.py"
+config.JobType.maxMemoryMB = 2500
 
 #### Data ####
-config.Data.inputDataset = "/RelValZEEMM_13_HI/CMSSW_7_5_3_patch1-75X_mcRun2_HeavyIon_v5-v1/GEN-SIM-DIGI-RAW-HLTDEBUG"
-#config.Data.inputDBS = "phys03"
+config.Data.inputDataset = "/ZEE_5TeV_GEN_SIM_PU/twang-ZEE_5TeV_GEN_SIM_PU_step2-f3950ab07bbd1716ce99d1da3e903b99/USER"
+config.Data.inputDBS = "phys03"
 config.Data.splitting = "FileBased"
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.outLFNDirBase = "/store/group/phys_heavyions/richard/HIHightPt/RelValZEEMM_13_HI_7_5_3_patch1-GlobalEcalReco/"
-config.Data.publishDataName = "RelValZEEMM_13_HI_7_5_3_patch1-GlobalEcalReco"
+config.Data.outLFNDirBase = "/store/user/richard/GlobalEcalRECO/ZEE_5TeV-GlobalEcalReco/"
+config.Data.publishDataName = "ZEE_5TeV-GlobalEcalReco"
 
 #### Site ####
-config.Site.storageSite = "T2_CH_CERN"
-#config.Site.whitelist = ["T2_US_MIT"]
-#config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
+config.Site.storageSite = "T2_US_MIT"
+config.Site.whitelist = ["T2_US_MIT"]
+config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
 #config.Site.blacklist = ["T0","T1"]
 
 #### User ####
