17ffac11cf01e7335278651ddf76fcefd4aaf30a
R. Alex Barbieri <luck@mit.edu>
2016-01-11 04:06:59 +0100

    pp settings

--- crabConfig.py
+++ crabConfig.py
@@ -3,18 +3,18 @@ config = config()
 
 #### JobType ####
 config.JobType.pluginName = "Analysis"
-config.JobType.psetName = "runForestAOD_PbPb_DATA_75X.py"
+config.JobType.psetName = "runForestAOD_pp_DATA_75X_wFilter.py"
 #config.JobType.maxMemoryMB = 2500
 #### Data ####
-config.Data.inputDataset = "/HIPhoton40AndZ/HIRun2015-Photon-PromptReco-v1/AOD"
-config.Data.lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/HI/DCSOnly/json_DCSONLY.txt"
+config.Data.inputDataset = "/HighPtPhoton30AndZ/Run2015E-PromptReco-v1/AOD"
+config.Data.lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/5TeV/Cert_262081-262273_5TeV_PromptReco_Collisions15_25ns_JSON_v2.txt"
 #config.Data.inputDBS = "phys03"
 config.Data.splitting = "FileBased"
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = False
 config.Data.outLFNDirBase = "/store/user/richard/2015-Data-photonSkims/"
-config.Data.outputDatasetTag = "PbPb-photonSkim-v1"
+config.Data.outputDatasetTag = "pp-photonSkim-wFilter-v0"
 
 #### Site ####
 config.Site.storageSite = "T2_US_MIT"
